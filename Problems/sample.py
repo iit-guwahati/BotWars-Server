b72c04cc5b37a145422c20e9c18af1a828aac6a4
Rajat Khanduja <rajatkhanduja13@gmail.com>
2013-02-28 13:21:31 +0530

    Added sample probem files and gitignore

---
+++ Problems/sample.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# Author : Rajat Khanduja
+#
+# Sample problem definition to show how to provide necessary information to the
+# server. This file demonstrates how to give the functions and constants
+# required by BotWarsHandler
+#
+# Required Constants
+#   testFiles : array of (inputFile, outputFile)
+#   MEM_LIM   : memory limit to be imposed on the running program
+#   TIME_LIM  : time limit to be imposed on the running program
+#
+# Required functions
+
+import filecmp
+
+testFiles = [("input_sample.txt", "output_sample.txt")]
+MEM_LIM   = 200000000 # bytes
+TIME_LIM  = 5 # seconds
+
+def evaluate(inputData, expectedOutput, producedOutput):
+  '''
+  Function returns a real number (integer or float) as a score. Higher the score,
+  better the performance.
+  
+  example :
+  if expectedOutput == producedOutput:
+    return 1
+  else:
+    return 0
+  
+  '''
+  if expectedOutput == producedOutput:
+    return 1
+  else:
+    return 0
