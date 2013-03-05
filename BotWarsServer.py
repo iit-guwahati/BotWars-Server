63bb7ee744c17250da32619140a30b2172aa7d4b
Suhail Sherif <suhail.sherif@gmail.com>
2013-03-05 22:34:05 +0530

    Generalized host-ip

--- BotWarsServer.py
+++ BotWarsServer.py
@@ -12,7 +12,7 @@ def usage():
 
 if __name__=='__main__':
   try:
-    HOST_NAME   = '172.16.25.187'
+    HOST_NAME   = '0.0.0.0'
     PORT_NUMBER = int(sys.argv[1])
   except Exception:
     usage()
