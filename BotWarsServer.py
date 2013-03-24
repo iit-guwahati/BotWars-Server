8cb9c3e170667a48404cd47d1dea1b3ea88ffb49
Rajat Khanduja <rajatkhanduja13@gmail.com>
2013-03-24 23:12:18 +0530

    License file added.

--- BotWarsServer.py
+++ BotWarsServer.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
-# Author : Rajat Khanduja <rajatkhanduja13@gmail.com>
+#
+# This file is part of the BotWarsServer program.
+# Copyright (C) 2013 Rajat Khanduja, Suhail Sherif
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# The program starts execution in this file.
 
 from BotWarsHandler import *
 from SocketServer import ThreadingMixIn
--- judge.py
+++ judge.py
@@ -1,8 +1,21 @@
-#!/usr/bin/python
-# Author : Rahul Huilgol <rahulrhuilgol@gmail.com>
-# 
-# This file defines functions required to compile and run the code and evaluate
-# the output
+# This file is part of the BotWarsServer program.
+# Copyright (C) 2013 Rahul Huilgol, Rajat Khanduja
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This file defines functions required to compile and run the code and 
+# evaluate the output
 # Currently supports only the following langauges
 #  - C
 #  - C++
